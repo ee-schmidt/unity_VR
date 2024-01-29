57a60122d3efb18b16f319790643710e781a8dcb
Vrushabh Deogirikar <vdeogirikar97@gmail.com>
2024-01-29 13:55:15 -0800

    Update AnimateHandController.cs

--- Assets/AnimateHandController.cs
+++ Assets/AnimateHandController.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Animator))]
 public class AnimateHandController : MonoBehaviour
 
-    public InputActionReference gripInputActionReference;
 {
+    public InputActionReference gripInputActionReference;
+    public InputActionReference triggerInputActionRefere;
+
+    private Animator _handAnimator;
+    private float _gripValue;
+    private float _triggerValue;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        _handAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        AnimateGrip();
+        AnimateTrigger();
     }
+
+    private void AnimateGrip() {
+        _gripValue = gripInputActionReference.action.ReadValue<float>();
+        _handAnimator.SetFloat("Grip", _gripValue);
+    }
+
+    private void AnimateTrigger() {
+        _triggerValue = triggerInputActionRefere.action.ReadValue<float>();
+        _handAnimator.SetFloat("Trigger", _triggerValue);
+    }
+
+
+
 }
